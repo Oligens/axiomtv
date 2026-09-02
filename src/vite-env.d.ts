b107af725ed37b721ccf71a233b8a16b8e4d8f36
Oligens <cleefolig@gmail.com>
2026-09-02 05:55:17 -0700

    chore(config): type public runtime environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,13 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_URL?: string;
+  readonly VITE_ZAKAPRO_SDK_URL?: string;
+  readonly VITE_ZAKAPRO_APP_KEY?: string;
+  readonly VITE_ZAKAPRO_HUB_URL?: string;
+  readonly VITE_ZAKAPRO_WEBHOOK_URL?: string;
+  readonly VITE_AGWE_OCEAN_AUDIO_URL?: string;
+  readonly VITE_AGWE_LOCALE?: string;
+  readonly VITE_AGWE_FPS?: string;
 }
 
 interface ImportMeta {
